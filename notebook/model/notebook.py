e22a309eec8a21d1441f1a780fede2128a05d8fc
mariajosehincapie18 <mjhincapie18@gmail.com>
2024-04-16 21:08:14 -0500

    correccion

--- notebook/model/notebook.py
+++ notebook/model/notebook.py
@@ -1,19 +1,18 @@
 from dataclasses import dataclass, field
-import datetime
-import uuid
+from _datetime import datetime
 from typing import ClassVar
 
 
 @dataclass
 class Note:
     code: int
-    text : str
-    importance : str
-    HIGH: ClassVar[str]= "HIGH"
+    text: str
+    importance: str
+    HIGH: ClassVar[str] = "HIGH"
     MEDIUM: ClassVar[str] = "MEDIUM"
     LOW:  ClassVar[str] = "LOW"
     tags: list[str] = field(init=False, default_factory=list)
-    creation_time: datetime.datetime = field(default_factory=datetime.datetime.now)
+    creation_date: datetime = field(init=False, default_factory=datetime.now)
 
 
 
@@ -23,9 +22,8 @@ class Note:
             self.tags.append(tag)
 
     def __str__(self) -> str:
-         return f" Code : {self.code}" \
-                f" creation date : {self.creation_date}" \
-                f" {self.title} : {self.text}"
+         return f" Code : {self.code}\nCreation date: {self.creation_date}\n{self.title}: {self.text}"
+
 
 
 
@@ -33,28 +31,28 @@ class Note:
 
 class Notebook:
     def __init__(self):
-        self.notes = dict[int, Note] = { }
-        pass
+        self.notes = dict[int, Note] = {}
+
 
     def add_note(self, title: str, text: str, importance: str):
-        new_note = Note(title, text)
-        new_note.code = str(uuid.uuid4())
-        self.notes.append(new_note)
-        return new_note.code
+        code: int = len(self.notes) + 1
+        note: Note = Note(code, title, text, importance)
+        self.notes[code] = note
+        return code
+
     def important_notes(self) -> list[Note]:
-        importance_tag = []
-        for x in self.tag :
-            if x  == "HIGH" or "MEDIUM":
-                x.append(importance_tag)
-        return importance_tag
+        return [note for note in self.notes.values() if note.importance == Note.HIGH or note.importance == Note.MEDIUM]
+
 
     def tags_note_count(self) -> dict[str, int]:
-        dict_tags_count = {}
-        for nota in self.notes:
-            if nota.tag:
-                for tag in nota.tag:
-                    dict_tags_count[tag] = dict_tags_count.get(tag,0) + 1
-        return dict_tags_count
+        tags_count = {}
+        for note in self.notes.values():
+            for tag in note.tags:
+                if tag not in tags_count:
+                    tags_count[tag] = 1
+                else:
+                    tags_count[tag] += 1
+        return tags_count
 
 
 
